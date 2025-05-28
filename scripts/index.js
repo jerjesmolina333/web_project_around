ad0f766535e963b5c23a85bdac89efe986d4dab0
jerjesmolina333 <jerjesmolina@gmail.com>
2025-05-27 20:20:35 -0600

    eventos del boton like y editar perfil

--- scripts/index.js
+++ scripts/index.js
@@ -1,64 +1,79 @@
-let textoAutor = document.querySelector(".profile__info__name");
-let profesion = document.querySelector(".profile__info__profession");
-let formaEdicion = document.querySelector(".popup");
-let botonesLike = document.querySelectorAll(".element__vec-container");
-let botonEditContainer = document.querySelector(
+const textoAutor = document.querySelector(".profile__info__name");
+const profesion = document.querySelector(".profile__info__profession");
+const formaEdicion = document.querySelector(".popup");
+// const botonesLike = document.querySelectorAll(".element__vec-container");
+const botonEditContainer = document.querySelector(
   ".profile__botonedit-container"
 );
-var botonEditar = document.querySelector(".profile__boton-edit");
-var imagenEditar = document.querySelector(".profile__plus");
-var botonGuardar = document.querySelector(".popup__button");
+const botonEditar = document.querySelector(".profile__boton-edit");
+const imagenEditar = document.querySelector(".profile__edit-image");
+const botonGuardar = document.querySelector(".popup__button");
+const imagenLike = document.querySelector(".element__like");
 
-formaEdicion.addEventListener("submit", (evt) => {
-  evt.preventDefault();
-  var nombre = formaEdicion.querySelector(".popup__name");
-  var profesión = formaEdicion.querySelector(".popup__profession");
+// Carga inicial del documento
+// document.addEventListener('DOMContentLoaded', function() {
 
-  var nombrePerfil = document.querySelector(".profile__name");
-  nombrePerfil.textContent = nombre.value;
-  var profesionPerfil = document.querySelector(".profile__profession");
-  profesionPerfil.textContent = profesión.value;
-  formaEdicion.style.display = "none";
+//Agregar los eventos para el botón botonEditar
+
+if (imagenEditar && imagenLike) {
+  const originalEditSrc = imagenEditar.src;
+  console.log("originalEditSrc:" + originalEditSrc);
+  // debugger;
+  imagenEditar.addEventListener("mouseenter", function () {
+    imagenEditar.src = "./images/EditButton2.png";
+  });
+  imagenEditar.addEventListener("mouseleave", function () {
+    imagenEditar.src = originalEditSrc;
+  });
+  imagenEditar.addEventListener("click", function () {
+    console.log("Desplegar edición");
+    formaEdicion.classList.add("popup_opened");
+    let nombre = document.querySelector(".profile__name");
+    var profesion = document.querySelector(".profile__profession");
+    var cajaTextoNombre = document.querySelector(".popup__name");
+    var textoNombre = document.getElementById("nombre");
+    textoNombre.value = nombre.textContent;
+
+    var textoAcerca = document.getElementById("acerca");
+    textoAcerca.value = profesion.textContent;
+  });
+
+  // const originalImagenLike = imagenLike.src;
+}
+
+document.querySelectorAll(".element__like").forEach(function (img) {
+  let liked = false;
+  console.log("actualizada la imagen like");
+  img.addEventListener("mouseenter", function () {
+    if (!liked) img.src = "./images/Like2.png";
+  });
+
+  img.addEventListener("mouseleave", function () {
+    if (liked) {
+      img.src = "./images/Like2.png";
+    } else {
+      img.src = "./images/Like.png";
+    }
+  });
+
+  img.addEventListener("click", function () {
+    liked = !liked;
+    if (liked) {
+      img.src = "./images/Like2.png";
+    } else {
+      img.src = "./images/Like.png";
+    }
+  });
 });
 
-function cambiaImagenEdit1() {
-  // alert("Se va a cambiar la imagen");
-  botonEditContainer.innerHTML = `
-            <button class="profile__boton-edit">
-            <img
-              src="./images/EditButton2.png"
-              class="profile__edit-image"
-              alt="Edit Button"
-            />
-          </button>
-          `;
-}
-
-function cambiaImagenEdit2() {
-  // alert("Se va a cambiar la imagen");
-  botonEditContainer.innerHTML = `
-            <button class="profile__boton-edit">
-            <img
-              src="./images/EditButton.png"
-              class="profile__edit-image"
-              alt="Edit Button"
-            />
-          </button>
-          `;
-}
-botonEditar.addEventListener("click", despliegaEdicionPerfil);
-imagenEditar.addEventListener("mouseover", cambiaImagenEdit1);
-imagenEditar.addEventListener("mouseout", cambiaImagenEdit2);
-
-// botonEditar.addEventListener("mouseleave", cambiaImagenEdit2);
-
-for (var i = 0; i < botonesLike.length; i++) {
-  botonesLike[i].addEventListener("mouseover", cambiaImagenLike);
-  // console.log("agregando el boton like " + i);
-}
-for (var i = 0; i < botonesLike.length; i++) {
-  botonesLike[i].addEventListener("mouseleave", regresaImagenLike);
-}
+// agregar los eventos para el botón like
+// for (let i = 0; i < botonesLike.length; i++) {
+//   botonesLike[i].addEventListener("mouseover", cambiaImagenLike);
+//   // console.log("agregando el boton like " + i);
+// }
+// for (let i = 0; i < botonesLike.length; i++) {
+//   botonesLike[i].addEventListener("mouseleave", regresaImagenLike);
+// }
 
 function cambiaImagenLike() {
   //   alert("Mouse over");
@@ -69,17 +84,29 @@ function regresaImagenLike() {
   this.classList.add("element__vec-container");
 }
 
+formaEdicion.addEventListener("submit", (evt) => {
+  evt.preventDefault();
+  let nombre = formaEdicion.querySelector(".popup__name");
+  let profesión = formaEdicion.querySelector(".popup__profession");
+
+  let nombrePerfil = document.querySelector(".profile__name");
+  nombrePerfil.textContent = nombre.value;
+  let profesionPerfil = document.querySelector(".profile__profession");
+  profesionPerfil.textContent = profesión.value;
+  formaEdicion.style.display = "none";
+});
+
 // Manejo del botón de edición de usuario:
 
 function despliegaEdicionPerfil() {
-  // alert("Desplagar edición");
+  alert("Desplegar edición");
   formaEdicion.classList.add("popup_opened");
-  var nombre = document.querySelector(".profile__name");
-  var profesion = document.querySelector(".profile__profession");
-  var cajaTextoNombre = document.querySelector(".popup__name");
-  var textoNombre = document.getElementById("nombre");
+  let nombre = document.querySelector(".profile__name");
+  let profesion = document.querySelector(".profile__profession");
+  let cajaTextoNombre = document.querySelector(".popup__name");
+  let textoNombre = document.getElementById("nombre");
   textoNombre.value = nombre.textContent;
 
-  var textoAcerca = document.getElementById("acerca");
+  let textoAcerca = document.getElementById("acerca");
   textoAcerca.value = profesion.textContent;
 }
