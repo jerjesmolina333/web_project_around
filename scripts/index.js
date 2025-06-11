63fc5f5ac1a3f74e575f58f33506aa1ff0f0d754
jerjesmolina333 <jerjesmolina@gmail.com>
2025-06-10 23:08:25 -0600

    Arreglos para la revisión humana 1

--- scripts/index.js
+++ scripts/index.js
@@ -1,5 +1,5 @@
 const profesion = document.querySelector(".profile__info__profession");
-const formaEdicion = document.querySelector(".popup");
+const formaEdicion = document.querySelector(".popup-edit-profile");
 const formaNewPlace = document.querySelector(".popup-new-place");
 const imagenEditar = document.querySelector(".profile__edit-image");
 // const imagenLike = document.querySelector(".element__like");
@@ -33,25 +33,42 @@ const initialCards = [
     link: "https://practicum-content.s3.us-west-1.amazonaws.com/new-markets/WEB_sprint_5/ES/lago.jpg",
   },
 ];
-
-function borraEstaTarjeta(evt) {
-  // debugger;
-  console.log("Elemento padre: " + this.parentElement);
-  // this.remove();
+function agregaPropsImg(img) {
+  img.addEventListener("click", function () {
+    const url = img.src;
+    // const elementPopup = document.querySelector("");
+    const imagenDespl = document.querySelector(".imagen__display");
+    const imagenTemplate = document.querySelector("#imagen").content;
+    const imagenElement = imagenTemplate
+      .querySelector(".popup")
+      .cloneNode(true);
+    imagenElement.querySelector(".imagen__pic").src = url;
+    imagenElement
+      .querySelector(".popup__cerrarIMG")
+      .addEventListener("click", function () {
+        // imagenDespl.classList.add("popup_hidden");
+        const padre1 = this.parentElement;
+        const padre2 = padre1.parentElement;
+        const padre3 = padre2.parentElement;
+        padre3.remove();
+      });
+    imagenDespl.append(imagenElement);
+  });
 }
 
 function createCard(card) {
-  // const cardsContainer = document.querySelector(".elements");
-  // debugger;
   const elementTemplate = document.querySelector("#element").content;
   const cardElement = elementTemplate
     .querySelector(".element-container")
     .cloneNode(true);
 
+  const imagen = cardElement.querySelector(".element__pic");
   cardElement.querySelector(".element__name").textContent = card.name;
   cardElement.querySelector(".element__pic").src = card.link;
   cardElement.querySelector(".element__pic").alt = card.name;
   // debugger;
+  agregaPropsImg(imagen);
+  // cardElement.querySelector(".element__pic").addEventListener("click");
   cardElement
     .querySelector(".element__trash")
     .addEventListener("click", function () {
@@ -66,9 +83,7 @@ function createCard(card) {
 
 function cargaImagenesInic(initialCards) {
   const cardsContainer = document.querySelector(".elements");
-  // debugger;
   initialCards.forEach((card) => {
-    // console.log("Nombre: " + card.name + " liga: " + card.link);
     // debugger;
     let cardElement = createCard(card);
     cardsContainer.append(cardElement);
@@ -81,66 +96,107 @@ function cargaImagenesInic(initialCards) {
 // Carga inicial del documento
 document.addEventListener("DOMContentLoaded", function () {
   cargaImagenesInic(initialCards);
+  const originalEditSrc = imagenEditar.src;
 
-  if (imagenEditar && imagenCerrarEP && imagenBotonPlus) {
-    const originalEditSrc = imagenEditar.src;
+  imagenBotonPlus.addEventListener("click", function () {
+    formaNewPlace.classList.add("popup_opened");
 
-    imagenBotonPlus.addEventListener("click", function () {
-      formaNewPlace.classList.add("popup_opened");
-    });
+    const contNP = document.querySelector(".popup-new-place");
+    const primerCampoTexto = formaNewPlace.querySelector(".popup__name");
+    primerCampoTexto.focus();
+    contNP.addEventListener(
+      "keydown",
+      (event) => {
+        var keyValue = event.key;
 
-    // Icono de editar perfil:
-    imagenEditar.addEventListener("mouseenter", function () {
-      // debugger;
-      imagenEditar.src = "./images/EditButton2.png";
-    });
+        if (keyValue == "Escape") {
+          formaNewPlace.classList.remove("popup_opened");
+          formaNewPlace.classList.add("popup_hidden");
+        }
+      },
+      false
+    );
+  });
+
+  imagenEditar.addEventListener("click", function () {
+    formaEdicion.classList.add("popup_opened");
+
+    let nombre = document.querySelector(".profile__name");
+    let profesion = document.querySelector(".profile__profession");
+    // let textoNombre = document.getElementById("nombre");
+    let textoNombre = document.querySelector(".popup__name");
+    textoNombre.value = nombre.textContent;
+
+    // let textoAcerca = document.getElementById("acerca");
+    let textoAcerca = document.querySelector(".popup__profession");
+    textoAcerca.value = profesion.textContent;
+
+    const contEP = document.querySelector(".popup-edit-profile");
+    const primerCampoTexto = document.querySelector(".popup__name");
+    primerCampoTexto.focus();
+    contEP.addEventListener(
+      "keydown",
+      (event) => {
+        var keyValue = event.key;
+
+        if (keyValue == "Escape") {
+          formaEdicion.classList.remove("popup_opened");
+          formaEdicion.classList.add("popup_hidden");
+        }
+      },
+      false
+    );
+  });
+
+  // Icono de editar perfil:
+  imagenEditar.addEventListener("mouseenter", function () {
     // debugger;
-    imagenEditar.addEventListener("mouseleave", function () {
-      imagenEditar.src = originalEditSrc;
-    });
-    imagenEditar.addEventListener("click", function () {
-      formaEdicion.classList.add("popup_opened");
-      let nombre = document.querySelector(".profile__name");
-      let profesion = document.querySelector(".profile__profession");
-      let textoNombre = document.querySelector(".popup__name");
-      textoNombre.value = nombre.textContent;
+    imagenEditar.src = "./images/EditButton2.png";
+  });
 
-      let textoAcerca = document.getElementById("acerca");
-      textoAcerca.value = profesion.textContent;
+  imagenEditar.addEventListener("mouseleave", function () {
+    imagenEditar.src = originalEditSrc;
+  });
+
+  imagenCerrarEP.addEventListener("click", function () {
+    formaEdicion.classList.remove("popup_opened");
+    formaEdicion.classList.add("popup_hidden");
+  });
+  imagenCerrarNP.addEventListener("click", function () {
+    formaNewPlace.classList.remove("popup_opened");
+    formaNewPlace.classList.add("popup_hidden");
+  });
+
+  // Este ciclo le agrega a cada imagen el evento para que al dar click
+  // en la imagen, se abra la ventana para verla completa:
+  document.querySelectorAll(".element__pic").forEach(function (img) {
+    agregaPropsImg(img);
+  });
+
+  document.querySelectorAll(".element__like").forEach(function (img) {
+    let liked = false;
+    img.addEventListener("mouseenter", function () {
+      if (!liked) img.src = "./images/Like2.png";
     });
 
-    imagenCerrarEP.addEventListener("click", function () {
-      formaEdicion.classList.remove("popup_opened");
-      formaEdicion.classList.add("popup_hidden");
+    img.addEventListener("mouseleave", function () {
+      if (liked) {
+        img.src = "./images/Like2.png";
+      } else {
+        img.src = "./images/Like.png";
+      }
     });
-    imagenCerrarNP.addEventListener("click", function () {
-      formaNewPlace.classList.remove("popup_opened");
-      formaNewPlace.classList.add("popup_hidden");
-    });
-    document.querySelectorAll(".element__like").forEach(function (img) {
-      let liked = false;
-      img.addEventListener("mouseenter", function () {
-        if (!liked) img.src = "./images/Like2.png";
-      });
 
-      img.addEventListener("mouseleave", function () {
-        if (liked) {
-          img.src = "./images/Like2.png";
-        } else {
-          img.src = "./images/Like.png";
-        }
-      });
-
-      img.addEventListener("click", function () {
-        liked = !liked;
-        if (liked) {
-          img.src = "./images/Like2.png";
-        } else {
-          img.src = "./images/Like.png";
-        }
-      });
+    img.addEventListener("click", function () {
+      liked = !liked;
+      if (liked) {
+        img.src = "./images/Like2.png";
+      } else {
+        img.src = "./images/Like.png";
+      }
     });
-  }
+  });
+
   //Agregar el evento Submit a la ventana emergente
 
   // formaEdicion.addEventListener("submit", (evt) => {
@@ -169,19 +225,4 @@ document.addEventListener("DOMContentLoaded", function () {
     formaNewPlace.classList.remove("popup_opened");
     formaNewPlace.classList.add("popup_hidden");
   });
-
-  // Manejo del botón de edición de usuario:
-
-  function despliegaEdicionPerfil() {
-    formaEdicion.classList.add("popup_opened");
-    let nombre = document.querySelector(".profile__name");
-    let profesion = document.querySelector(".profile__profession");
-    // let textoNombre = document.getElementById("nombre");
-    let textoNombre = document.querySelector(".popup__name");
-    textoNombre.value = nombre.textContent;
-
-    // let textoAcerca = document.getElementById("acerca");
-    let textoAcerca = document.querySelector(".popup__profession");
-    textoAcerca.value = profesion.textContent;
-  }
 });
