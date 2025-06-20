7fbb62667bfe7bdc767039436a404525d228addc
jerjesmolina333 <jerjesmolina@gmail.com>
2025-06-20 10:40:23 -0600

    Cambios intentando que se pueda abrir la ventana de edición

--- scripts/index.js
+++ scripts/index.js
@@ -1,111 +1,159 @@
-const profesion = document.querySelector(".profile__info__profession");
-const formaEdicion = document.querySelector(".popup-edit-profile");
-const formaNewPlace = document.querySelector(".popup-new-place");
-const imagenEditar = document.querySelector(".profile__edit-image");
-// const imagenLike = document.querySelector(".element__like");
-const imagenCerrarEP = document.querySelector(".popup__cerrarEP");
-const imagenCerrarNP = document.querySelector(".popup__cerrarNP");
-const imagenBotonPlus = document.querySelector(".profile__plus");
-let imagenDesplegada = false;
-
-const initialCards = [
-  {
-    name: "Valle de Yosemite",
-    link: "https://practicum-content.s3.us-west-1.amazonaws.com/new-markets/WEB_sprint_5/ES/yosemite.jpg",
-  },
-  {
-    name: "Lago Louise",
-    link: "https://practicum-content.s3.us-west-1.amazonaws.com/new-markets/WEB_sprint_5/ES/lake-louise.jpg",
-  },
-  {
-    name: "Montañas Calvas",
-    link: "https://practicum-content.s3.us-west-1.amazonaws.com/new-markets/WEB_sprint_5/ES/bald-mountains.jpg",
-  },
-  {
-    name: "Latemar",
-    link: "https://practicum-content.s3.us-west-1.amazonaws.com/new-markets/WEB_sprint_5/ES/latemar.jpg",
-  },
-  {
-    name: "Parque Nacional de la Vanoise",
-    link: "https://practicum-content.s3.us-west-1.amazonaws.com/new-markets/WEB_sprint_5/ES/vanoise.jpg",
-  },
-  {
-    name: "Lago di Braies",
-    link: "https://practicum-content.s3.us-west-1.amazonaws.com/new-markets/WEB_sprint_5/ES/lago.jpg",
-  },
-];
-function agregaPropsImg(img) {
-  img.addEventListener("click", function () {
-    if (!imagenDesplegada) {
-      const url = img.src;
-
-      const imagenDespl = document.querySelector(".imagen__display");
-      const imagenTemplate = document.querySelector("#imagen").content;
-      const imagenElement = imagenTemplate
-        .querySelector(".popup")
-        .cloneNode(true);
-      imagenElement.querySelector(".imagen__pic").src = url;
-      imagenElement
-        .querySelector(".popup__cerrarIMG")
-        .addEventListener("click", function () {
-          const padre1 = this.parentElement;
-          const padre2 = padre1.parentElement;
-          const padre3 = padre2.parentElement;
-          const padre4 = padre3.parentElement;
-
-          padre1.remove();
-          padre2.remove();
-          padre3.remove();
-          imagenDesplegada = false;
-          return;
-        });
-      imagenDespl.append(imagenElement);
-      imagenDesplegada = true;
-    }
-  });
-}
-
-function createCard(card) {
-  const elementTemplate = document.querySelector("#element").content;
-  const cardElement = elementTemplate
-    .querySelector(".element__container")
-    .cloneNode(true);
-
-  const imagen = cardElement.querySelector(".element__pic");
-  cardElement.querySelector(".element__name").textContent = card.name;
-  cardElement.querySelector(".element__pic").src = card.link;
-  cardElement.querySelector(".element__pic").alt = card.name;
-  // debugger;
-  agregaPropsImg(imagen);
-  imagenDesplegada = false;
-
-  cardElement
-    .querySelector(".element__trash")
-    .addEventListener("click", function () {
-      const padre1 = this.parentElement;
-      const padre2 = padre1.parentElement;
-      // debugger;
-      padre2.remove();
-    });
-
-  return cardElement;
-}
-
-function cargaImagenesInic(initialCards) {
-  const cardsContainer = document.querySelector(".elements");
-  initialCards.forEach((card) => {
-    let cardElement = createCard(card);
-    cardsContainer.append(cardElement);
-  });
-}
-
 // ===========================
 // ===========================
 // ===========================
 // Carga inicial del documento
 document.addEventListener("DOMContentLoaded", function () {
+  const profesion = document.querySelector(".profile__info__profession");
+  const formaEdicion = document.querySelector(".popup-edit-profile");
+  const formaNewPlace = document.querySelector(".popup-new-place");
+  const imagenEditar = document.querySelector(".profile__edit-image");
+  // const imagenLike = document.querySelector(".element__like");
+  const imagenCerrarEP = document.querySelector(".popup__cerrarEP");
+  const imagenCerrarNP = document.querySelector(".popup__cerrarNP");
+  const imagenBotonPlus = document.querySelector(".profile__plus");
+  const editButton = document.querySelector(".profile__boton-edit");
+  let imagenDesplegada = false;
+
+  const initialCards = [
+    {
+      name: "Valle de Yosemite",
+      link: "https://practicum-content.s3.us-west-1.amazonaws.com/new-markets/WEB_sprint_5/ES/yosemite.jpg",
+    },
+    {
+      name: "Lago Louise",
+      link: "https://practicum-content.s3.us-west-1.amazonaws.com/new-markets/WEB_sprint_5/ES/lake-louise.jpg",
+    },
+    {
+      name: "Montañas Calvas",
+      link: "https://practicum-content.s3.us-west-1.amazonaws.com/new-markets/WEB_sprint_5/ES/bald-mountains.jpg",
+    },
+    {
+      name: "Latemar",
+      link: "https://practicum-content.s3.us-west-1.amazonaws.com/new-markets/WEB_sprint_5/ES/latemar.jpg",
+    },
+    {
+      name: "Parque Nacional de la Vanoise",
+      link: "https://practicum-content.s3.us-west-1.amazonaws.com/new-markets/WEB_sprint_5/ES/vanoise.jpg",
+    },
+    {
+      name: "Lago di Braies",
+      link: "https://practicum-content.s3.us-west-1.amazonaws.com/new-markets/WEB_sprint_5/ES/lago.jpg",
+    },
+  ];
+  function agregaPropsImg(img) {
+    img.addEventListener("click", function () {
+      if (!imagenDesplegada) {
+        const url = img.src;
+
+        const imagenDespl = document.querySelector(".imagen__display");
+        const imagenTemplate = document.querySelector("#imagen").content;
+        const imagenElement = imagenTemplate
+          .querySelector(".popup")
+          .cloneNode(true);
+        imagenElement.querySelector(".imagen__pic").src = url;
+        imagenElement
+          .querySelector(".popup__cerrarIMG")
+          .addEventListener("click", function () {
+            const padre1 = this.parentElement;
+            const padre2 = padre1.parentElement;
+            const padre3 = padre2.parentElement;
+            const padre4 = padre3.parentElement;
+
+            padre1.remove();
+            padre2.remove();
+            padre3.remove();
+            imagenDesplegada = false;
+            return;
+          });
+        imagenDespl.append(imagenElement);
+        imagenDesplegada = true;
+      }
+    });
+  }
+
+  function createCard(card) {
+    const elementTemplate = document.querySelector("#element").content;
+    const cardElement = elementTemplate
+      .querySelector(".element__container")
+      .cloneNode(true);
+
+    const imagen = cardElement.querySelector(".element__pic");
+    cardElement.querySelector(".element__name").textContent = card.name;
+    cardElement.querySelector(".element__pic").src = card.link;
+    cardElement.querySelector(".element__pic").alt = card.name;
+    // debugger;
+    agregaPropsImg(imagen);
+    imagenDesplegada = false;
+
+    cardElement
+      .querySelector(".element__trash")
+      .addEventListener("click", function () {
+        const padre1 = this.parentElement;
+        const padre2 = padre1.parentElement;
+        // debugger;
+        padre2.remove();
+      });
+
+    return cardElement;
+  }
+
+  function cargaImagenesInic(initialCards) {
+    const cardsContainer = document.querySelector(".elements");
+    initialCards.forEach((card) => {
+      let cardElement = createCard(card);
+      cardsContainer.append(cardElement);
+    });
+  }
+
+  function procesaClickEditarPerfil() {
+    formaEdicion.classList.add("popup_opened");
+    // debugger;
+    let nombre = document.querySelector(".profile__name");
+    let profesion = document.querySelector(".profile__profession");
+    let textoNombre = document.querySelector(".popup__name");
+    textoNombre.value = nombre.textContent;
+
+    let textoAcerca = document.querySelector(".popup__profession");
+    textoAcerca.value = profesion.textContent;
+
+    const contEP = document.querySelector(".popup-edit-profile");
+    const primerCampoTexto = document.querySelector(".popup__name");
+    primerCampoTexto.focus();
+    contEP.addEventListener(
+      "keydown",
+      (event) => {
+        var keyValue = event.key;
+
+        if (keyValue == "Escape") {
+          formaEdicion.classList.remove("popup_opened");
+          formaEdicion.classList.add("popup_hidden");
+        }
+      },
+      false
+    );
+  }
+
+  function attendSubmitProfile(evt) {
+    evt.preventDefault();
+    let nombre = formaEdicion.querySelector(".popup__name");
+    let profesión = formaEdicion.querySelector(".popup__profession");
+    let nombrePerfil = document.querySelector(".profile__name");
+
+    nombrePerfil.textContent = nombre.value;
+    let profesionPerfil = document.querySelector(".profile__profession");
+    profesionPerfil.textContent = profesión.value;
+
+    debugger;
+    editButton.removeEventListener("click", procesaClickEditarPerfil);
+    editButton.addEventListener("click", procesaClickEditarPerfil);
+    formaEdicion.removeEventListener("submit", attendSubmitProfile);
+
+    formaEdicion.style.display = "none";
+  }
+
+  ////////////////////////////////
+  //// CÓDIGO:
   cargaImagenesInic(initialCards);
-  const originalEditSrc = imagenEditar.src;
 
   imagenBotonPlus.addEventListener("click", function () {
     formaNewPlace.classList.add("popup_opened");
@@ -140,45 +188,21 @@ document.addEventListener("DOMContentLoaded", function () {
     imagenBotonPlus.src = "./images/BotonCerrar.png";
   });
 
-  imagenEditar.addEventListener("click", function () {
-    formaEdicion.classList.add("popup_opened");
+  if (imagenEditar) {
+    const originalEditSrc = imagenEditar.src;
+    imagenEditar.addEventListener("mouseenter", function () {
+      // debugger;
+      imagenEditar.src = "./images/EditButton2.png";
+    });
 
-    let nombre = document.querySelector(".profile__name");
-    let profesion = document.querySelector(".profile__profession");
-    // let textoNombre = document.getElementById("nombre");
-    let textoNombre = document.querySelector(".popup__name");
-    textoNombre.value = nombre.textContent;
+    imagenEditar.addEventListener("mouseleave", function () {
+      imagenEditar.src = originalEditSrc;
+    });
+  }
 
-    // let textoAcerca = document.getElementById("acerca");
-    let textoAcerca = document.querySelector(".popup__profession");
-    textoAcerca.value = profesion.textContent;
-
-    const contEP = document.querySelector(".popup-edit-profile");
-    const primerCampoTexto = document.querySelector(".popup__name");
-    primerCampoTexto.focus();
-    contEP.addEventListener(
-      "keydown",
-      (event) => {
-        var keyValue = event.key;
-
-        if (keyValue == "Escape") {
-          formaEdicion.classList.remove("popup_opened");
-          formaEdicion.classList.add("popup_hidden");
-        }
-      },
-      false
-    );
-  });
-
-  // Icono de editar perfil:
-  imagenEditar.addEventListener("mouseenter", function () {
-    // debugger;
-    imagenEditar.src = "./images/EditButton2.png";
-  });
-
-  imagenEditar.addEventListener("mouseleave", function () {
-    imagenEditar.src = originalEditSrc;
-  });
+  if (editButton) {
+    editButton.addEventListener("click", procesaClickEditarPerfil);
+  }
 
   imagenCerrarEP.addEventListener("click", function () {
     formaEdicion.classList.remove("popup_opened");
@@ -214,19 +238,9 @@ document.addEventListener("DOMContentLoaded", function () {
   });
 
   //Agregar el evento Submit a la ventana emergente
+  // de edición del perfil:
 
-  // formaEdicion.addEventListener("submit", (evt) => {
-  formaEdicion.addEventListener("submit", function (evt) {
-    evt.preventDefault();
-    let nombre = formaEdicion.querySelector(".popup__name");
-    let profesión = formaEdicion.querySelector(".popup__profession");
-
-    let nombrePerfil = document.querySelector(".profile__name");
-    nombrePerfil.textContent = nombre.value;
-    let profesionPerfil = document.querySelector(".profile__profession");
-    profesionPerfil.textContent = profesión.value;
-    formaEdicion.style.display = "none";
-  });
+  formaEdicion.addEventListener("submit", attendSubmitProfile);
 
   formaNewPlace.addEventListener("submit", function (evt) {
     // debugger;
