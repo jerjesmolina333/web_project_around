cef8b15e7c64768026fca545db3de3f483934cc1
jerjesmolina333 <jerjesmolina@gmail.com>
2025-06-23 13:01:46 -0600

    debugger eliminado

--- scripts/index.js
+++ scripts/index.js
@@ -43,7 +43,6 @@ document.addEventListener("DOMContentLoaded", function () {
   ];
   function agregaPropsImg(img) {
     img.addEventListener("click", function () {
-      debugger;
       if (!imagenDesplegada) {
         const url = img.src;
 
